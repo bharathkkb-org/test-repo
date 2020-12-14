07f0ae6fea5d718c9c72ceae6e752d1fb58129c4
Bharath KKB <bharathkrishnakb@gmail.com>
2020-12-13 23:34:57 -0600

    test (#7)

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    target = os.environ.get('TARGET', 'Github')
-    return 'Hello {}!\n'.format(target)
+    target = os.environ.get('TARGET', 'GitHub')
+    return render_template("index.html", content=f"Hello {target}")
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
